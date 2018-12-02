01a2c0cd8bdf3b07d7286a27e90265d519b40428
Simon Wolf <simon@simonwolf.io>
2018-12-02 12:08:57 +0000

    Tidied the day01 code to be file stream-based.

--- lib/advent_of_code_2018/day_01.ex
+++ lib/advent_of_code_2018/day_01.ex
@@ -1,20 +1,18 @@
 defmodule AdventOfCode2018.Day01 do
-  def part1(args) do
-    args
-    |> String.split("\n")
-    |> Enum.map(fn x -> String.to_integer(x) end)
+  def part1(file_path) do
+    file_path
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(fn x -> String.to_integer(x) end)
     |> Enum.sum()
   end
 
-  def part2(args) do
-    args
-    |> String.split("\n")
-    |> Enum.map(fn x -> String.to_integer(x) end)
+  def part2(file_path) do
+    file_path
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(fn x -> String.to_integer(x) end)
     |> Stream.cycle()
-    # |> Enum.reduce_while({0, [0]}, fn x, {current_frequency, recorded_frequencies} ->
-    #   new_frequency = current_frequency + x
-    #   if new_frequency in recorded_frequencies, do: {:halt, new_frequency}, else: {:cont, {new_frequency, recorded_frequencies ++ [new_frequency] }}
-    # end)
     |> Enum.reduce_while({0, MapSet.new([0])}, fn x, {current_frequency, recorded_frequencies} ->
       new_frequency = current_frequency + x
       if MapSet.member?(recorded_frequencies, new_frequency), do: {:halt, new_frequency}, else: {:cont, {new_frequency, MapSet.put(recorded_frequencies, new_frequency)}}
--- test/advent_of_code_2018/day_01_test.exs
+++ test/advent_of_code_2018/day_01_test.exs
@@ -4,50 +4,38 @@ defmodule AdventOfCode2018.Day01Test do
   import AdventOfCode2018.Day01
 
   test "part1" do
-    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_01_test_01.txt"
-    {:ok, items} = File.read(file_path)
-    result = part1(items)
-
+    file_path = "./data/day_01_task_01_test_01.txt"
+    result = part1(file_path)
     assert result == 3
   end
 
   test "part2-01" do
-    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_01.txt"
-    {:ok, items} = File.read(file_path)
-    result = part2(items)
-
+    file_path = "./data/day_01_task_02_test_01.txt"
+    result = part2(file_path)
     assert result == 2
   end
 
   test "part2-02" do
-    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_02.txt"
-    {:ok, items} = File.read(file_path)
-    result = part2(items)
-
+    file_path = "./data/day_01_task_02_test_02.txt"
+    result = part2(file_path)
     assert result == 0
   end
 
   test "part2-03" do
-    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_03.txt"
-    {:ok, items} = File.read(file_path)
-    result = part2(items)
-
+    file_path = "./data/day_01_task_02_test_03.txt"
+    result = part2(file_path)
     assert result == 10
   end
 
   test "part2-04" do
-    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_04.txt"
-    {:ok, items} = File.read(file_path)
-    result = part2(items)
-
+    file_path = "./data/day_01_task_02_test_04.txt"
+    result = part2(file_path)
     assert result == 5
   end
 
   test "part2-05" do
-    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_05.txt"
-    {:ok, items} = File.read(file_path)
-    result = part2(items)
-
+    file_path = "./data/day_01_task_02_test_05.txt"
+    result = part2(file_path)
     assert result == 14
   end
 
