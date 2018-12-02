f7f7d9f67abff08b75664a9ce94471356dda9d8e
Simon Wolf <simon@simonwolf.io>
2018-12-02 00:40:39 +0000

    Solution to day 1’s challenge.

--- lib/advent_of_code_2018/day_01.ex
+++ lib/advent_of_code_2018/day_01.ex
@@ -1,7 +1,23 @@
 defmodule AdventOfCode2018.Day01 do
   def part1(args) do
+    args
+    |> String.split("\n")
+    |> Enum.map(fn x -> String.to_integer(x) end)
+    |> Enum.sum()
   end
 
   def part2(args) do
+    args
+    |> String.split("\n")
+    |> Enum.map(fn x -> String.to_integer(x) end)
+    |> Stream.cycle()
+    # |> Enum.reduce_while({0, [0]}, fn x, {current_frequency, recorded_frequencies} ->
+    #   new_frequency = current_frequency + x
+    #   if new_frequency in recorded_frequencies, do: {:halt, new_frequency}, else: {:cont, {new_frequency, recorded_frequencies ++ [new_frequency] }}
+    # end)
+    |> Enum.reduce_while({0, MapSet.new([0])}, fn x, {current_frequency, recorded_frequencies} ->
+      new_frequency = current_frequency + x
+      if MapSet.member?(recorded_frequencies, new_frequency), do: {:halt, new_frequency}, else: {:cont, {new_frequency, MapSet.put(recorded_frequencies, new_frequency)}}
+    end)
   end
 end
--- test/advent_of_code_2018/day_01_test.exs
+++ test/advent_of_code_2018/day_01_test.exs
@@ -3,19 +3,52 @@ defmodule AdventOfCode2018.Day01Test do
 
   import AdventOfCode2018.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil 
-    result = part1(input)
+    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_01_test_01.txt"
+    {:ok, items} = File.read(file_path)
+    result = part1(items)
 
-    assert result
+    assert result == 3
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil 
-    result = part2(input)
+  test "part2-01" do
+    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_01.txt"
+    {:ok, items} = File.read(file_path)
+    result = part2(items)
 
-    assert result
+    assert result == 2
   end
+
+  test "part2-02" do
+    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_02.txt"
+    {:ok, items} = File.read(file_path)
+    result = part2(items)
+
+    assert result == 0
+  end
+
+  test "part2-03" do
+    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_03.txt"
+    {:ok, items} = File.read(file_path)
+    result = part2(items)
+
+    assert result == 10
+  end
+
+  test "part2-04" do
+    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_04.txt"
+    {:ok, items} = File.read(file_path)
+    result = part2(items)
+
+    assert result == 5
+  end
+
+  test "part2-05" do
+    file_path = "/Users/simon/Development/Elixir/advent-of-code/data/day_01_task_02_test_05.txt"
+    {:ok, items} = File.read(file_path)
+    result = part2(items)
+
+    assert result == 14
+  end
+
 end
