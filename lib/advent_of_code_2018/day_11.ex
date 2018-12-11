7d49a08c6ffc39f2a9910dd085d5257916c25b50
Simon Wolf <simon@ottersoftware.com>
2018-12-11 22:49:39 +0000

    First pass at Day 11.

--- lib/advent_of_code_2018/day_11.ex
+++ lib/advent_of_code_2018/day_11.ex
@@ -1,7 +1,166 @@
 defmodule AdventOfCode2018.Day11 do
-  def part1(args) do
+  @moduledoc false
+
+  @doc """
+  --- Day 11: Chronal Charge ---
+
+  You watch the Elves and their sleigh fade into the distance as they head 
+  toward the North Pole.
+
+  Actually, you're the one fading. The falling sensation returns.
+
+  The low fuel warning light is illuminated on your wrist-mounted device. 
+  Tapping it once causes it to project a hologram of the situation: a 300x300 
+  grid of fuel cells and their current power levels, some negative. You're not 
+  sure what negative power means in the context of time travel, but it can't 
+  be good.
+
+  Each fuel cell has a coordinate ranging from 1 to 300 in both the X 
+  (horizontal) and Y (vertical) direction. In X,Y notation, the top-left cell 
+  is 1,1, and the top-right cell is 300,1.
+
+  The interface lets you select any 3x3 square of fuel cells. To increase your 
+  chances of getting to your destination, you decide to choose the 3x3 square 
+  with the largest total power.
+
+  The power level in a given fuel cell can be found through the following 
+  process:
+
+  - Find the fuel cell's rack ID, which is its X coordinate plus 10.
+  - Begin with a power level of the rack ID times the Y coordinate.
+  - Increase the power level by the value of the grid serial number (your 
+    puzzle input).
+  - Set the power level to itself multiplied by the rack ID.
+  - Keep only the hundreds digit of the power level (so 12345 becomes 3; 
+    numbers with no hundreds digit become 0).
+  - Subtract 5 from the power level.
+
+  For example, to find the power level of the fuel cell at 3,5 in a grid 
+  with serial number 8:
+
+  - The rack ID is 3 + 10 = 13.
+  - The power level starts at 13 * 5 = 65.
+  - Adding the serial number produces 65 + 8 = 73.
+  - Multiplying by the rack ID produces 73 * 13 = 949.
+  - The hundreds digit of 949 is 9.
+  - Subtracting 5 produces 9 - 5 = 4.
+  
+  So, the power level of this fuel cell is 4.
+
+  Here are some more example power levels:
+
+  - Fuel cell at  122,79, grid serial number 57: power level -5.
+  - Fuel cell at 217,196, grid serial number 39: power level  0.
+  - Fuel cell at 101,153, grid serial number 71: power level  4.
+
+  Your goal is to find the 3x3 square which has the largest total power. The 
+  square must be entirely within the 300x300 grid. Identify this square using 
+  the X,Y coordinate of its top-left fuel cell. For example:
+
+  For grid serial number 18, the largest total 3x3 square has a top-left 
+  corner of 33,45 (with a total power of 29); these fuel cells appear in the 
+  middle of this 5x5 region:
+
+  -2  -4   4   4   4
+  -4   4   4   4  -5
+   4   3   3   4  -4
+   1   1   2   4  -3
+  -1   0   2  -5  -2
+
+  For grid serial number 42, the largest 3x3 square's top-left is 21,61 (with 
+  a total power of 30); they are in the middle of this region:
+
+  -3   4   2   2   2
+  -4   4   3   3   4
+  -5   3   3   4  -4
+   4   3   3   4  -3
+   3   3   3  -5  -1
+
+  What is the X,Y coordinate of the top-left fuel cell of the 3x3 square with 
+  the largest total power?
+  """
+  def part1(serial_number) do
+    get_best_square(serial_number, 3, 3)
   end
 
-  def part2(args) do
+
+  @doc """
+  You discover a dial on the side of the device; it seems to let you select a 
+  square of any size, not just 3x3. Sizes from 1x1 to 300x300 are supported.
+
+  Realizing this, you now must find the square of any size with the largest 
+  total power. Identify this square by including its size as a third parameter 
+  after the top-left coordinate: a 9x9 square with a top-left corner of 3,5 is 
+  identified as 3,5,9.
+
+  For example:
+
+  - For grid serial number 18, the largest total square (with a total power of 
+    113) is 16x16 and has a top-left corner of 90,269, so its identifier is 
+    90,269,16.
+  - For grid serial number 42, the largest total square (with a total power of 
+    119) is 12x12 and has a top-left corner of 232,251, so its identifier is 
+    232,251,12.
+
+  What is the X,Y,size identifier of the square with the largest total power?
+  """
+  def part2(serial_number) do
+    get_best_square(serial_number, 1, 300)
   end
+
+  defp get_best_square(serial_number, grid_min_size, grid_max_size) do
+    grid = build_grid(serial_number)
+
+    Enum.reduce(grid_min_size..grid_max_size, {{0, 0}, 0, 0}, fn size, acc ->
+      IO.puts "Squares of " <> Integer.to_string(size) <> "..."
+      Enum.reduce(1..300 - size + 1, acc, fn x, acc -> 
+        Enum.reduce(1..300 - size + 1, acc, fn y, acc -> 
+          # IO.puts "Get area for " <> Integer.to_string(x) <> ", " <> Integer.to_string(y) <> " (" <> Integer.to_string(size) <> ")"
+          total = get_area_total(grid, x, y, size)
+
+          {{best_x, best_y}, best_size, best_total} = acc
+          if total > best_total do
+            {{x, y}, size, total}
+          else
+            {{best_x, best_y}, best_size, best_total}
+          end
+        end)
+      end)
+    end)
+  end
+
+  defp get_area_total(grid, x, y, size) do
+    Enum.reduce(x..x + size - 1, 0, fn x_coord, acc ->
+      Enum.reduce(y..y + size - 1, acc, fn y_coord, acc ->
+        acc + Map.get(grid, {x_coord, y_coord})
+      end)
+    end)
+  end
+
+  defp build_grid(serial_number) do
+    Enum.reduce(1..300, %{}, fn x, acc -> 
+      Enum.reduce(1..300, acc, fn y, acc -> 
+        Map.put(acc, {x, y}, cell_power_level({x, y}, serial_number))
+      end)
+    end)
+  end
+
+  defp cell_power_level({x, y}, serial_number) do
+    rack_id = x + 10
+
+    power_level = rack_id * y
+    power_level = power_level + serial_number
+    power_level = power_level * rack_id
+
+    power_level = if power_level > 99 do
+      Integer.to_string(power_level)
+      |> String.slice(-3, 1)
+      |> String.to_integer()
+    else
+      0
+    end
+
+    power_level - 5
+  end
+
 end
--- test/advent_of_code_2018/day_11_test.exs
+++ test/advent_of_code_2018/day_11_test.exs
@@ -1,14 +1,15 @@
 defmodule AdventOfCode2018.Day11Test do
   use ExUnit.Case
+  doctest AdventOfCode2018.Day11
 
   import AdventOfCode2018.Day11
 
-  @tag :skip
-  test "part1" do
-    input = nil 
-    result = part1(input)
+  test "part1 with serial number 18" do
+    assert {{33, 45}, 3, 29} = part1(18)
+  end
 
-    assert result
+  test "part1 with serial number 42" do
+    assert {{21, 61}, 3, 30} = part1(42)
   end
 
   @tag :skip
