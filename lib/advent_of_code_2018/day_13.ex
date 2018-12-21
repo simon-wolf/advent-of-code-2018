bced3a2cce3eed04bccddf667a889b7c3c9e191a
Simon Wolf <simon@ottersoftware.com>
2018-12-21 00:24:17 +0000

    Completed Day 13.
    Includes refactoring to use most of the part 2 code for part 1.

--- lib/advent_of_code_2018/day_13.ex
+++ lib/advent_of_code_2018/day_13.ex
@@ -196,7 +196,7 @@ defmodule AdventOfCode2018.Day13 do
   def part1(file_path) do
     {track_elements, carts_info} = parse_data_file(file_path)
 
-    {x, y} = move_carts_until_crash(track_elements, carts_info)
+    move_carts_until_crash(track_elements, carts_info)
   end
 
   @doc """
@@ -251,7 +251,7 @@ defmodule AdventOfCode2018.Day13 do
   def part2(file_path) do
     {track_elements, carts_info} = parse_data_file(file_path)
 
-    {x, y} = move_carts_ignoring_crashes(track_elements, carts_info)
+    move_carts_allowing_crashes(track_elements, carts_info)
   end
 
   # Create two maps; one containing the track elements and the other 
@@ -294,40 +294,42 @@ defmodule AdventOfCode2018.Day13 do
   # characters
   defp parse_map_character(" ", _x, _y, track_elements, carts_info), do: {track_elements, carts_info}
   defp parse_map_character("", _x, _y, track_elements, carts_info), do: {track_elements, carts_info}
-  defp parse_map_character("<" = character, x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "-"), Map.put(carts_info, {x, y}, {:left, :junction_go_left})}
-  defp parse_map_character(">" = character, x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "-"), Map.put(carts_info, {x, y}, {:right, :junction_go_left})}
-  defp parse_map_character("^" = character, x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "|"), Map.put(carts_info, {x, y}, {:up, :junction_go_left})}
-  defp parse_map_character("v" = character, x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "|"), Map.put(carts_info, {x, y}, {:down, :junction_go_left})}
+  defp parse_map_character("<", x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "-"), Map.put(carts_info, {x, y}, {:left, :junction_go_left})}
+  defp parse_map_character(">", x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "-"), Map.put(carts_info, {x, y}, {:right, :junction_go_left})}
+  defp parse_map_character("^", x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "|"), Map.put(carts_info, {x, y}, {:up, :junction_go_left})}
+  defp parse_map_character("v", x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, "|"), Map.put(carts_info, {x, y}, {:down, :junction_go_left})}
   defp parse_map_character(character, x, y, track_elements, carts_info), do: {Map.put(track_elements, {x, y}, character), carts_info}
 
+  # Get the coordinates of the first crash
   defp move_carts_until_crash(track_elements, carts_info) do
-    sorted_cart_keys = sorted_cart_identifiers(carts_info)
-
-    case first_crash_in_tick(sorted_cart_keys, carts_info) do
-      {_, {-1, -1}} -> 
+    case any_crash_in_tick(carts_info) do
+      {:no_crash} ->
         new_carts_info = update_cart_positions(track_elements, carts_info)
         move_carts_until_crash(track_elements, new_carts_info)
-      {_, {x, y}} ->
-        {x, y}
+      {:crash, _cart_1_coords, _cart_2_coords, crash_coords} ->
+        crash_coords
     end
   end
 
-  defp move_carts_ignoring_crashes(track_elements, carts_info) do
-    sorted_cart_keys = sorted_cart_identifiers(carts_info)
+  # Get the coordinates of the last cart left after all crashes
+  defp move_carts_allowing_crashes(track_elements, carts_info) do
+    case any_crash_in_tick(carts_info) do
+      {:no_crash} ->
+        new_carts_info = update_cart_positions(track_elements, carts_info)
 
-    trimmed_carts_info = crashed_carts(sorted_cart_keys, carts_info)
-    |> Enum.reduce(carts_info, fn crashed_coords, acc ->
-      Map.delete(acc, crashed_coords)
-    end)
+        if map_size(new_carts_info) == 1 do
+          new_carts_info
+          |> Map.keys()
+          |> List.first()
+        else
+          move_carts_allowing_crashes(track_elements, new_carts_info)
+        end
+      {:crash, cart_1_coords, cart_2_coords, _crash_coords} ->
+        new_carts_info = carts_info
+        |> Map.delete(cart_1_coords)
+        |> Map.delete(cart_2_coords)
 
-    new_carts_info = update_cart_positions(track_elements, trimmed_carts_info)
-
-    # If there is only one cart left then return the key which is its
-    # coordinates, otherwise carry on ticking
-    if Map.keys(new_carts_info) |> length() == 1 do
-      Map.keys(new_carts_info) |> List.first()
-    else
-      move_carts_ignoring_crashes(track_elements, new_carts_info)
+        move_carts_allowing_crashes(track_elements, new_carts_info)
     end
   end
 
@@ -339,7 +341,7 @@ defmodule AdventOfCode2018.Day13 do
       {direction, next_turn_direction} = Map.get(carts_info, {x, y})
       track_element_character = Map.get(track_elements, {new_x, new_y})
       
-      acc = case track_element_character do
+      case track_element_character do
         "-" ->
           Map.put(acc, {new_x, new_y}, {direction, next_turn_direction})
         "|" ->
@@ -396,48 +398,54 @@ defmodule AdventOfCode2018.Day13 do
     end)
   end
 
-  # Returns x and y where crash occurs or {-1, -1} if no crash
-  defp first_crash_in_tick(sorted_cart_keys, carts_info) do
-    Enum.reduce_while(sorted_cart_keys, {Map.new(carts_info), {-1, -1}}, fn {x, y}, {new_carts_info, {crash_x, crash_y}} = acc ->
-      {new_x, new_y} = new_coords_for_cart(carts_info, {x, y})
-      {direction, next_turn_direction} = Map.get(carts_info, {x, y})
+  defp any_crash_in_tick(carts_info) do
+    # Build a map of coords for each cart. The key is the current (original) 
+    # coordinateand which is then replaced by the coord the cart will move to.
+    # The value is always the original coordinate. It is this way around since
+    # it is easier to check a map for a particular key when looking for crashes.
+    coords_map = Map.keys(carts_info)
+    |> Enum.reduce(%{}, fn coords, acc ->
+      Map.put(acc, coords, coords)
+    end)
 
-      if Map.has_key?(new_carts_info, {new_x, new_y}) == true do
-        {:halt, {acc, {new_x, new_y}}}
+    sorted_cart_keys = sorted_cart_identifiers(carts_info)
+    result = Enum.reduce_while(sorted_cart_keys, Map.new(coords_map), fn current_cart_coords, acc ->
+      new_coords = new_coords_for_cart(carts_info, current_cart_coords)
+      if (Map.has_key?(acc, new_coords)) do
+        {:halt, { :crash, current_cart_coords, Map.get(acc, new_coords), new_coords}}
       else
-        modified_carts_info = new_carts_info
-        |> Map.delete({x, y})
-        |> Map.put({new_x, new_y}, {direction, next_turn_direction})
+        new_acc = acc
+        |> Map.delete(current_cart_coords)
+        |> Map.put(new_coords, current_cart_coords)
+  
+        {:cont, new_acc}
+      end
+    end)
 
-        {:cont, {modified_carts_info, {crash_x, crash_y}}}
+    case result do
+      {:crash, cart_1_coords, cart_2_coords, crash_coords} ->
+        result
+      _ ->
+        {:no_crash}
+    end
+  end  
+
+  defp sorted_cart_identifiers(carts_info) do
+    Map.keys(carts_info)
+    |> sorted_coordinates
+  end
+
+  defp sorted_coordinates(coordinates) do
+    coordinates
+    |> Enum.sort(fn {x1, y1}, {x2, y2} ->
+      cond do
+        y1 < y2 -> true
+        y1 > y2 -> false
+        x1 < x2 -> true
+        true -> false
       end
     end)
   end
-
-  # Returns a list of cart coordinates for carts which _will_ crash.
-  # These are not their new coordinates but rather their current ones at the 
-  # start of the tick
-  defp crashed_carts(sorted_cart_keys, carts_info) do
-    # sorted_cart_keys = sorted_cart_identifiers(carts_info)
-
-    Enum.reduce(sorted_cart_keys, MapSet.new(), fn {x, y}, acc ->
-      {new_x, new_y} = new_coords_for_cart(carts_info, {x, y})
-
-      # If the acc already contains the cart's coordinates then skip it
-      if MapSet.member?(acc, {x, y}) do
-        acc
-      else
-        if Map.has_key?(carts_info, {new_x, new_y}) == true do
-          acc
-          |> MapSet.put({x, y})
-          |> MapSet.put({new_x, new_y})
-        else
-          acc
-        end
-      end
-    end)
-    |> MapSet.to_list()
-  end  
 
   defp new_coords_for_cart(carts_info, {x, y} = cart_key) do
     {direction, _next_turn_direction} = Map.get(carts_info, cart_key)
@@ -448,18 +456,6 @@ defmodule AdventOfCode2018.Day13 do
       :down -> {x, y + 1}
       _ -> {x, y}
     end
-  end
-
-  defp sorted_cart_identifiers(carts_info) do
-    Map.keys(carts_info)
-    |> Enum.sort(fn {x1, y1}, {x2, y2} ->
-      cond do
-        y1 < y2 -> true
-        y1 > y2 -> false
-        x1 < x2 -> true
-        true -> false
-      end
-    end)
   end
 
 end
